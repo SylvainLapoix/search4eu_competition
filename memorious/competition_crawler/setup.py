d2258b57e8ac77af041cd0a030ad9337afc66a61
sy <sylvainlapoix@gmail.com>
2021-02-24 13:30:14 +0100

    ajout competition_crawler

---
+++ memorious/competition_crawler/setup.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from setuptools import setup, find_packages
+
+setup(
+    name="competition_crawler",
+    version=datetime.utcnow().date().isoformat(),
+    classifiers=[],
+    keywords="",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["memorious", "datafreeze"],
+    entry_points={"memorious.plugins": ["example = example:init"]},
+)
